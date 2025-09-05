f5a3a25edd04db644845270d2bb3169a8b76f486
Artur Khutak <arty4prof@gmail.com>
2025-09-05 14:56:25 +0300

    Extend integration tests and update readme.md (#13)
    * update readme and demo library deps
    * cover examples wordnik api with integration test

---
+++ Tests/Wordnik.Client.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Wordnik.Client.Helpers;
+
+namespace Wordnik.Client.IntegrationTests;
+
+public abstract class IntegrationTestBase
+{
+    protected readonly string _apiKey;
+    protected readonly HttpClient _httpClient;
+    protected readonly IConfiguration _configuration;
+
+    protected IntegrationTestBase()
+    {
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        _apiKey = _configuration["Wordnik:ApiKey"]
+                 ?? throw new InvalidOperationException("API Key is missing from configuration.");
+
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(WordnikConstants.WordnikApiUrl)
+        };
+    }
+}
