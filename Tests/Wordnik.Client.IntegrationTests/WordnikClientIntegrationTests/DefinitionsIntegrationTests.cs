cd4e8f80fabaf875596c86e73d0ac9ed324cd43d
Artur Khutak <arty4prof@gmail.com>
2025-09-05 14:54:20 +0300

    cover examples wordnik api with integration test

---
+++ Tests/Wordnik.Client.IntegrationTests/WordnikClientIntegrationTests/DefinitionsIntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using Wordnik.Client.Requests;
+
+namespace Wordnik.Client.IntegrationTests.WordnikClientIntegrationTests;
+
+public class DefinitionsIntegrationTests : IntegrationTestBase
+{
+    [Theory]
+    [InlineData("example", 3)]
+    public async Task GetDefinitions_WhenCalled_ShouldReturnDefinitions(string word, int limit)
+    {
+        // Arrange
+        var client = new WordnikClient(_httpClient, _apiKey);
+
+        var request = new GetDefinitionsRequest
+        {
+            Word = word,
+            Limit = limit
+        };
+
+        // Act
+        var definitions = await client.GetDefinitionsAsync(request);
+
+        // Assert
+        Assert.NotNull(definitions);
+        Assert.NotEmpty(definitions);
+        Assert.True(definitions.Count() == limit);
+
+        foreach (var definition in definitions)
+        {
+            Assert.False(string.IsNullOrWhiteSpace(definition.Text), "Definition text is missing.");
+            Assert.NotNull(definition.PartOfSpeech);
+            Assert.NotNull(definition.Word);
+            Assert.NotNull(definition.WordnikUrl);
+        }
+    }
+}
---
+++ Tests/Wordnik.Client.IntegrationTests/WordnikClientIntegrationTests/ExamplesIntegrationTests.cs
@@ -0,0 +1,41 @@
+﻿using Wordnik.Client.Requests;
+
+namespace Wordnik.Client.IntegrationTests.WordnikClientIntegrationTests;
+
+public class ExamplesIntegrationTests : IntegrationTestBase
+{
+    [Theory]
+    [InlineData("example", 3)]
+    public async Task GetDefinitions_WhenCalled_ShouldReturnDefinitions(string word, int limit)
+    {
+        // Arrange
+        var client = new WordnikClient(_httpClient, _apiKey);
+
+        var request = new GetExamplesRequest
+        {
+            Word = word,
+            Limit = limit
+        };
+
+        // Act
+        var examples = await client.GetExamplesAsync(request);
+
+        // Assert
+        Assert.NotNull(examples);
+        Assert.NotEmpty(examples.Examples);
+        Assert.True(examples.Examples.Count == limit);
+
+        foreach (var example in examples.Examples)
+        {
+            Assert.False(string.IsNullOrWhiteSpace(example.Text), "Definition text is missing.");
+            Assert.True(example.Url.Length > 0);
+            Assert.True(example.Word.Length > 0);
+            Assert.True(example.Title.Length > 0);
+            Assert.NotNull(example.Year);
+            Assert.NotNull(example.Provider?.Id);
+            Assert.True(example.DocumentId > 0);
+            Assert.True(example.Rating > 0);
+            Assert.True(example.ExampleId > 0);
+        }
+    }
+}
