ace2d0a1384c8b799967d297da014e6a8c14ffcc
Artur Khutak <arty4prof@gmail.com>
2025-09-05 11:49:13 +0300

    minor fixes

--- Wordnik.Client/WordnikClient.cs
+++ Wordnik.Client/WordnikClient.cs
@@ -58,7 +58,7 @@ namespace Wordnik.Client
 
             if (string.IsNullOrWhiteSpace(request.Word))
             {
-                throw new ArgumentException("Word cannot be null or empty.", nameof(IWord.Word));
+                throw new ArgumentException("Word cannot be null or empty.", nameof(request));
             }
 
             var url = $"word.json/{Uri.EscapeDataString(request.Word)}/{apiPath}?{request}";
